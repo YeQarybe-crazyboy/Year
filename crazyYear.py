45cf31797f7c2eca22265067a756dff48a2f4a2e
YeQarybe-crazyboy <72519484+YeQarybe-crazyboy@users.noreply.github.com>
2020-10-21 02:40:49 +0330

    Add files via upload

---
+++ crazyYear.py
@@ -0,0 +1,17 @@
+
+a = int(input("مقدار سال را برای تبدیل وارد کنید! >>> "))
+
+print(f"{a} year = \n\nsecond = ",a*31557600,"\n")
+
+print("Minutes = ",a*31557600/60,"\n")
+
+print("hours = ",a*31557600/60/60,"\n")
+
+print("Day = ",a*31557600/60/60/24,"\n")
+
+print("Week = ",a*31557600/60/60/24/7,"\n")
+
+print("Month = ",a*31557600/60/60/24/7/4+2,"\n")
+
+print("My chanell is :\n@PowerBot_TM")
+
